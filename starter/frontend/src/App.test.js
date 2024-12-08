080f5daacc3328b1e0f05f522a671c6944d5acd2
hamen262 <hamen.work@gmail.com>
2024-12-08 06:46:19 +0000

    add App.test.js

---
+++ starter/frontend/src/App.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders movie', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Movie/i);
+  expect(linkElement).toBeInTheDocument();
+});
