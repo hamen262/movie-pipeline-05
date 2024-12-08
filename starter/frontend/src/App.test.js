ff0edbe31dbfc355f082be3575edef61676a2f7e
Ha Thi Thuy Men <hamen.work@gmail.com>
2024-12-08 14:12:42 +0700

    update source

--- starter/frontend/src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders movie', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Movie/i);
-  expect(linkElement).toBeInTheDocument();
-});
