9e5afdb7d4d6f8c296b34fb3ea4d359644624fe0
hamen262 <hamen.work@gmail.com>
2024-12-18 16:12:23 +0000

    debug

--- starter/frontend/src/components/MovieList.js
+++ starter/frontend/src/components/MovieList.js
@@ -4,10 +4,12 @@ import axios from 'axios';
 
 function MovieList({ onMovieClick }) {
   const [movies, setMovies] = useState([]);
-
+ 
   useEffect(() => {
     axios.get(`${process.env.REACT_APP_MOVIE_API_URL}/movies`).then((response) => {
       setMovies(response.data.movies);
+      console.log(movies);
+
     });
   }, []);
 
