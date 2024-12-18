30557b9bddbf9b3dbef91121bd29d12195f213a6
hamen262 <hamen.work@gmail.com>
2024-12-18 16:29:56 +0000

    test build

--- starter/frontend/src/components/MovieList.js
+++ starter/frontend/src/components/MovieList.js
@@ -1,31 +1,21 @@
-import React, { useState, useEffect } from "react";
-import PropTypes from "prop-types";
-import axios from "axios";
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import axios from 'axios';
 
 function MovieList({ onMovieClick }) {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    console.log("API URL:", process.env.REACT_APP_MOVIE_API_URL);
-
-    axios
-      .get(`${process.env.REACT_APP_MOVIE_API_URL}/movies`)
-      .then((response) => {
-        setMovies(response.data.movies);
-      })
-      .catch((error) => {
-        console.error("Error fetching movies:", error);
-      });
+    axios.get(`${process.env.REACT_APP_MOVIE_API_URL}/movies`).then((response) => {
+      setMovies(response.data.movies);
+    });
   }, []);
 
   return (
     <ul>
-      {movies.map((movie) => (
-        <li
-          className="movieItem"
-          key={movie.id}
-          onClick={() => onMovieClick(movie)}
-        >
+      {
+      movies?.map((movie) => (
+        <li className="movieItem" key={movie.id} onClick={() => onMovieClick(movie)}>
           {movie.title}
         </li>
       ))}
